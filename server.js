c5eefb480fe8fd0c44f08e490f58c918aee7c54b
prathibhabugga <prathibhabugga2400@gmail.com>
2026-03-03 09:41:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env,PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+const PORT = process.env,PORT || 3000;
 app.listen(PORT, () => {
   console.log('Bloom server running on http://localhost:${PORT}');
 
 });
+
 
